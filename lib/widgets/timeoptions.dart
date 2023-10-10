7ffbf71ebb207d42416d376a02561c6bef665537
Cesar Delgado <cesardelgadom@outlook.com>
2023-10-09 22:19:56 -0500

    implementacion timecard y timeoptions

---
+++ lib/widgets/timeoptions.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TimeOptions extends StatelessWidget {
+  const TimeOptions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: selectableTimes.map((time) {
+          return Container(
+            margin: EdgeInsets.only(left: 10),
+            width: 70,
+            height: 50,
+            decoration: BoxDecoration(
+                border: Border.all(width: 3, color: Colors.white30)),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
